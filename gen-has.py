98a963ffa40949dc9ad62cf13e59720a255df0df
MatiGarWszystkoZajete <mateus.garbien@gmail.com>
2023-11-09 10:23:17 +0100

    kalkulator 1.0

---
+++ gen-has.py
@@ -0,0 +1,26 @@
+# Hasla
+import random
+dlugoschasla = 0
+LiteraM = random.choice("qwertyuiopasdfghjklzxcvbnm")
+LiteraD = random.choice("QWERTYUIOPASDFGHJKLZXCVBNM")
+cyfry = random.choice("1234567890")
+
+haslo = []
+
+
+if dlugoschasla < 8:
+    dlugoschasla = int(input("Wypisz dlugosc hasla (min 8): "))
+
+while dlugoschasla != 0:    
+    typ = random.randint(0,3)
+    if typ == 1:
+        typ = LiteraM
+    elif typ == 2:
+        typ = LiteraD
+    elif typ == 3:
+        typ = cyfry
+    haslo.append(typ)
+    dlugoschasla-=1
+
+print("Twoje haslo: ")
+print(*haslo, sep="")
